013137dc50c7d583a15bf0a49aa31c922212d15a
AwkAmr <es-amr.attia2027@alexu.edu.eg>
2026-09-12 14:37:02 -0400

    fix: fixed error

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
+
   allowedDevOrigins: ['10.55.98.86'],
 };
 
